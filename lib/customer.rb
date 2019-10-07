d980013ccf01865cd01ffe7bd3935c968d1078e6
Srikalyan Megati <kalmegati@gmail.com>
2019-10-07 19:55:19 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,15 @@
 class Customer
+    
+  attr_accessor :waiter, :customer, :total, :tip
+  
+  @@all = []
+  
+  def initialize(waiter, customer, total, tip = 0)
+    
+  end
+  
+  def self.all
+    @@all
+  end
   
 end
