2ce942c2456998a6fa9c3ed7eeb1039fc97087e5
Srikalyan Megati <kalmegati@gmail.com>
2019-10-07 19:55:14 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,5 +1,15 @@
 class Meal
   
+  attr_accessor :waiter, :customer, :total, :tip
   
+  @@all = []
+  
+  def initialize(waiter, customer, total, tip = 0)
+    
+  end
+  
+  def self.all
+    @@all
+  end
   
 end
